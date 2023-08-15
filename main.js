3fee9df59fbeb6ec10d42bba186ae6cb3dfc65f1
Dom dK <dom@devildog.co.za>
2023-08-15 12:35:12 +0200

    commit validation

---
+++ main.js
@@ -0,0 +1,59 @@
+// const notifySubmit = document.querySelector('button');
+
+// const input = document.querySelector('.emailAddress');
+
+// let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+// const email = document.detElementById(emailAddress);
+// const errorMessage = document.createElement('p');
+
+// notifySubmit.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     if (input.value.match(regex)) {
+//         return (true);
+//     } else {
+// errorMessage = "Please provide valid email address";
+// input.appendChild(errorMessage);
+// errorMessage.classList.add('invalidEmail');
+//         alert('invalid email');
+
+//     }
+
+// })
+
+
+// function validateEmail(email) {
+//     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+//         return (true);
+//     } else {
+//         errorMessage = "Please provide valid email address";
+//         input.appendChild(errorMessage);
+//         errorMessage.classList.add('invalidEmail');
+//     }
+// }
+
+const output = document.getElementById('output');
+const submitEmail = document.querySelector('button');
+
+
+submitEmail.addEventListener("click", function (e) {
+    e.preventDefault();
+
+
+    let userEmail = document.getElementById('emailInput').value;
+    let regex = new RegExp(/\S+@\S+\.\S+/);
+    let isValid = regex.test(userEmail);
+
+
+    if (isValid) {
+        // return true;
+        console.log('true')
+    } else {
+        output.classList.add("invalidEmailMessage")
+        emailInput.classList.add("invalidEmail");
+        output.innerHTML = "Please provide a valid email address";
+
+    }
+})
+
+
